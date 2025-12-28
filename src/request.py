f83c0d546d964adcb9f5a0d129d3424507dc026d
Ubay <ubaycool3@gmail.com>
2025-12-28 00:17:17 -0800

    Add basic SupportRequest class with description, state, and ID

---
+++ src/request.py
@@ -0,0 +1,14 @@
+request_counter = 0
+
+class SupportRequest:
+    def __init__(self, description):
+        # Set the description and state
+        self.description = description
+        self.state = "submitted"  
+
+        # Increment the request counter and set the ID
+        global request_counter
+        request_counter += 1
+        self.id = request_counter
+
+   
---
+++ src/test.py
@@ -0,0 +1,6 @@
+from request import SupportRequest
+
+req = SupportRequest("My computer is not working")
+print(req.description)
+print(req.state) 
+print(req.id)
